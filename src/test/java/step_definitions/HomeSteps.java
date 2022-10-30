532d921f87c36d58c695750a5b89347943375875
imae19 <ms.chaijan@gmail.com>
2022-10-30 12:47:34 -0400

    add USIN-1

--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -1,11 +1,16 @@
 package step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import pages.CommonPage;
 import pages.HomePage;
 import utils.BrowserUtils;
+
+import java.util.Map;
 
 public class HomeSteps implements CommonPage {
     HomePage page;
@@ -19,10 +24,33 @@ public class HomeSteps implements CommonPage {
     public void i_open_url_of_homepage() {
         BrowserUtils.getDriver();
     }
+
     @Then("Verify header text is {string}")
     public void verify_header_text_is(String headerTitle) {
-            BrowserUtils.assertEquals(BrowserUtils.getDriver().getTitle(),headerTitle);
+        BrowserUtils.assertEquals(BrowserUtils.getDriver().getTitle(), headerTitle);
+    }
+
+    @When("I fill out login form with following details:")
+    public void iFillOutLoginFormWithFollowingDetails(Map<String, String> map) {
+        for (String key : map.keySet()) {
+            BrowserUtils.sendKeys(
+                    BrowserUtils.getDriver().findElement(By.name(key)), map.get(key)
+            );
         }
+    }
+
+    @When("I click a button {string}")
+    public void iClickAButton(String button) {
+        BrowserUtils.click(BrowserUtils.getDriver().findElement(
+                By.xpath(String.format(XPATH_TEMPLATE_BUTTON, button))));
+    }
+
+    @Then("Verify I can login successfully")
+    public void verify_i_can_login_successfully() {
+        BrowserUtils.assertTrue(true);
+    }
 }
+
+
 
 
--- src/test/resources/features/home.feature
+++ src/test/resources/features/home.feature
@@ -1,8 +1,13 @@
 Feature: Home page tests
 
-Background: Navigate to Home Page
-Given I open url of homepage
+  Background: Navigate to Home Page
+    Given I open url of homepage
+    Then Verify header text is "Interview App"
 
-@IN-1 @regression
-Scenario: Test header of the home page
-Then Verify header text is "Interview App"
+  @IN-1 @regression
+  Scenario: Contact Us form test
+    When I fill out login form with following details:
+      | email    | test@yahoo.com |
+      | password | test123        |
+    When I click a button "Login"
+    Then Verify I can login successfully
